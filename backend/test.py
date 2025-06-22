137f7fea1ba52f8a0aeba67a365c6b175867ab5e
Leor Porat <porato18@gmail.com>
2025-06-22 11:44:45 -0700

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,6 @@
 from SoothSayer import SoothSayer
 import os
 
-client = SoothSayer(groq_api_key='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', midas_model_type='DPT_Large')
+client = SoothSayer(groq_api_key='KEY', midas_model_type='DPT_Large')
 
 client.get_speech_from_text("Hello world!")
