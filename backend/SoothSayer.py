6ae792162e7f2222de257c9327aea172eec103af
mhtruong1031 <mhtruong1031@gmail.com>
2025-06-21 19:09:05 -0700

    balls

--- backend/SoothSayer.py
+++ backend/SoothSayer.py
@@ -1,21 +1,34 @@
-import cv2
-import torch
+from groq import Groq
+
+import cv2, torch
+import numpy as np
 
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 import speech_recognition as sr
 
-from groq import Groq
+from vedo import Points, show
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 class SoothSayer:
-    def __init__(self, midas_model_type: str):
-        self.client     = Groq()
+    # model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+    # model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+    # model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+    def __init__(self, groq_api_key: str, midas_model_type: str):
+        self.client     = Groq(api_key=groq_api_key)
         self.recognizer = sr.Recognizer()
 
         # MiDaS initialization
         self.midas = torch.hub.load("intel-isl/MiDaS", midas_model_type)
+        self.midas.to(device)
+        self.midas.eval()
+
+        midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+        if midas_model_type == "DPT_Large" or midas_model_type == "DPT_Hybrid":
+            self.transform = midas_transforms.dpt_transform
+        else:
+            self.transform = midas_transforms.small_transform
 
     def input_to_audio(self, image_front, image_back, audio) -> None:
         speech = self.recognizer.listen(audio)
@@ -52,3 +65,46 @@ class SoothSayer:
 
         for chunk in completion:
             print(chunk.choices[0].delta.content or "", end="")
+    
+    def image_to_projection(self,image):
+        #img = cv2.imdecode(np.array(image))
+        img = cv2.imread(image)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        input_batch = self.transform(img).to(device)
+
+        with torch.no_grad():
+            prediction = self.midas(input_batch)
+
+            prediction = torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=img.shape[:2],
+                mode="bicubic",
+                align_corners=False,
+            ).squeeze()
+
+        output = prediction.cpu().numpy()
+
+        h, w = output.shape
+
+        x = np.flip(np.tile(np.arange(w), h)/40)
+        x = x - x.mean()
+        y = -np.flip(output.flatten()) + 38
+        z = np.repeat(np.arange(h), w)/40
+
+        
+        xyz = np.stack((x, y, z), axis=1)
+
+        y = xyz[:, 1]
+
+        # Compute threshold for top 20%
+        # threshold = np.percentile(y, 80)
+        # mask = y >= threshold
+        # xyz = xyz[mask]
+        
+        pts = Points(xyz, r=4)  # r is point radius
+        pts.cmap("viridis", xyz[:, 1])  # color by y-values (you can change this)
+        show(pts, axes=1, bg='white', title='3D Point Cloud')
+
+
+        
